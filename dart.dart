c4c1616945ad81779b7ab25fafd68fca740240b1
Md_Echo <allanodiwuor54@gmail.com>
2024-03-25 15:02:15 +0300

    first commit

---
+++ dart.dart
@@ -0,0 +1,15 @@
+void main() {
+  int num1 = 10;//whole number
+  double num2 = 10.22;//floating number
+  String StudentName = "Allan Obuya";//text value stored in a string
+  List<String> Subjects = ["Kiswahili", "English", "Maths"];// list containing strings only
+  Map<String,int> Grades = {"Kiswahili": 90,"English": 70,"Maths": 50}; // a list containing different values
+  bool isStudentPassed = true;//it answers the question has the student passed?
+  print("Student Name is $StudentName");
+  print("age is $num1");
+  print("weight is $num2");
+  print("subject is $Subjects");
+  print("grades is $Grades");
+  print("Has student passed is $isStudentPassed");
+
+}
